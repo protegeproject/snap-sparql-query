3e80ff53d1551637ce4cdcf460173417bdab1400
Matthew Horridge <matthew.horridge@stanford.edu>
2015-06-05 17:42:01 -0700

    Fixes for LANG builtin

--- src/main/java/org/semanticweb/owlapi/sparql/builtin/eval/LANG_Evaluator.java
+++ src/main/java/org/semanticweb/owlapi/sparql/builtin/eval/LANG_Evaluator.java
@@ -7,11 +7,21 @@ import org.semanticweb.owlapi.sparql.api.*;
  * Stanford Center for Biomedical Informatics Research
  * 10/03/15
  */
-public class LANG_Evaluator extends AbstractUnaryStringLiteralBuiltInCallEvaluator {
+public class LANG_Evaluator extends AbstractUnaryBuiltInCallEvaluator {
 
     @Override
-    protected EvaluationResult evaluate(Literal literal, SolutionMapping sm) {
-        String lang = literal.getLang();
-        return EvaluationResult.getResult(Literal.createString(lang));
+    protected EvaluationResult evaluate(Expression arg, SolutionMapping sm) {
+        EvaluationResult result = arg.evaluateAsLiteral(sm);
+        if(result.isError()) {
+            return result;
+        }
+        Literal literal = result.asLiteral();
+        if(literal.isRDFPlainLiteral()) {
+            String lang = literal.getLang();
+            return EvaluationResult.getResult(Literal.createRDFPlainLiteralNoLang(lang));
+        }
+        else {
+            return EvaluationResult.getError();
+        }
     }
 }
