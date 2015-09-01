a559ede2cd919e8a026bd313bbf883a47de7209d
Matthew Horridge <matthew.horridge@stanford.edu>
2015-09-01 16:04:57 -0700

    Optimisation to MinusEvaluator

---
+++ src/main/java/org/semanticweb/owlapi/sparql/sparqldl/MinusEvaluator.java
@@ -0,0 +1,92 @@
+package org.semanticweb.owlapi.sparql.sparqldl;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.LinkedHashMultiset;
+import org.semanticweb.owlapi.sparql.api.RDFTerm;
+import org.semanticweb.owlapi.sparql.api.SolutionMapping;
+import org.semanticweb.owlapi.sparql.api.Variable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Matthew Horridge Stanford Center for Biomedical Informatics Research 07/06/15
+ */
+public class MinusEvaluator {
+
+    private final ImmutableList<SolutionMapping> leftSolutionSequence;
+
+    private final ImmutableList<SolutionMapping> rightSolutionSequence;
+
+    private final ImmutableList<Variable> sharedVariables;
+
+    public MinusEvaluator(ImmutableList<SolutionMapping> leftSolutionSequence, ImmutableList<SolutionMapping> rightSolutionSequence, Set<Variable> sharedVariables) {
+        this.leftSolutionSequence = leftSolutionSequence;
+        this.rightSolutionSequence = rightSolutionSequence;
+        this.sharedVariables = ImmutableList.copyOf(sharedVariables);
+    }
+
+    public ImmutableList<SolutionMapping> getMinus() {
+        if (sharedVariables.isEmpty()) {
+            return leftSolutionSequence;
+        }
+
+        LinkedHashMultiset<Key> rightKeys = LinkedHashMultiset.create();
+
+        rightSolutionSequence.stream()
+                .map(sm -> new Key(sharedVariables, sm))
+                .forEach(rightKeys::add);
+
+        ImmutableList.Builder<SolutionMapping> resultBuilder = ImmutableList.builder();
+
+        leftSolutionSequence.stream()
+                .map(sm -> new Key(sharedVariables, sm))
+                .filter(key -> !rightKeys.contains(key))
+                .map(Key::getSolutionMapping)
+                .forEach(resultBuilder::add);
+
+        return resultBuilder.build();
+    }
+
+    private static class Key {
+
+        private final List<Optional<RDFTerm>> bindings;
+
+        private final SolutionMapping solutionMapping;
+
+        public Key(ImmutableList<Variable> sharedVariables, SolutionMapping sm) {
+            this.bindings = new ArrayList<>(sharedVariables.size());
+            this.solutionMapping = sm;
+            for (Variable variable : sharedVariables) {
+                bindings.add(sm.getTermForVariable(variable));
+            }
+        }
+
+        public SolutionMapping getSolutionMapping() {
+            return solutionMapping;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(bindings);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) {
+                return true;
+            }
+            if (!(obj instanceof Key)) {
+                return false;
+            }
+            Key other = (Key) obj;
+            return bindings.equals(other.bindings);
+        }
+    }
+}
