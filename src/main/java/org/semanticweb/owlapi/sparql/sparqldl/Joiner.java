5fd794eacd30c1218496aacdc3bdf29fe487c155
Matthew Horridge <matthew.horridge@stanford.edu>
2015-09-01 18:40:02 -0700

    Better implementation of LeftJoin

--- src/main/java/org/semanticweb/owlapi/sparql/sparqldl/Joiner.java
+++ src/main/java/org/semanticweb/owlapi/sparql/sparqldl/Joiner.java
@@ -1,14 +1,12 @@
 package org.semanticweb.owlapi.sparql.sparqldl;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.base.*;
+import com.google.common.collect.*;
 import org.semanticweb.owlapi.sparql.api.RDFTerm;
 import org.semanticweb.owlapi.sparql.api.SolutionMapping;
 import org.semanticweb.owlapi.sparql.api.Variable;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Matthew Horridge Stanford Center for Biomedical Informatics Research 07/06/15
@@ -25,33 +23,52 @@ public class Joiner {
         compatibilityChecker = new CompatibilityChecker(joinVariables);
     }
 
-    public List<SolutionMapping> getLeftJoin() {
-        List<SolutionMapping> result = new ArrayList<>(leftSolutionSequence.size());
-        for(SolutionMapping left : leftSolutionSequence) {
-            boolean joined = false;
-            for(SolutionMapping right : rightSolutionSequence) {
-                if(compatibilityChecker.isCompatible(left, right)) {
-                    joined = true;
-                    HashMap<Variable, RDFTerm> combined = new HashMap<>();
-                    combined.putAll(left.asMap());
-                    combined.putAll(right.asMap());
-                    SolutionMapping join = new SolutionMapping(ImmutableMap.copyOf(combined));
-                    result.add(join);
+    public ImmutableList<SolutionMapping> getLeftJoin() {
+        int expectedKeys = rightSolutionSequence.size();
+        int expectedValuesPerKey = 1;
+
+        Multimap<CompatibilityKey, SolutionMapping> rightMap = ArrayListMultimap.create(expectedKeys, expectedValuesPerKey);
+
+        ImmutableList<Variable> sharedVariables = ImmutableList.copyOf(compatibilityChecker.getVariables());
+
+        rightSolutionSequence.stream()
+                .map(sm -> new CompatibilityKey(sharedVariables, sm))
+                .forEach(key -> rightMap.put(key, key.getSolutionMapping()));
+
+        ImmutableList.Builder<SolutionMapping> result = ImmutableList.builder();
+        for (SolutionMapping left : leftSolutionSequence) {
+            CompatibilityKey leftKey = new CompatibilityKey(sharedVariables, left);
+            Collection<SolutionMapping> compatibleSolutions = rightMap.get(leftKey);
+            for (SolutionMapping right : compatibleSolutions) {
+                // NB ImmutableMap.Builder doesn't let us have multiple entries
+                HashMap<Variable, RDFTerm> combined = new HashMap<>();
+                for(Variable leftVariable : left.getVariables()) {
+                    com.google.common.base.Optional<RDFTerm> leftTerm = left.getTermForVariable(leftVariable);
+                    if (leftTerm.isPresent()) {
+                        combined.put(leftVariable, leftTerm.get());
+                    }
                 }
+                for(Variable rightVariable : right.getVariables()) {
+                    com.google.common.base.Optional<RDFTerm> rightTerm = right.getTermForVariable(rightVariable);
+                    if (rightTerm.isPresent()) {
+                        combined.put(rightVariable, rightTerm.get());
+                    }
+                }
+                SolutionMapping join = new SolutionMapping(ImmutableMap.copyOf(combined));
+                result.add(join);
             }
-            if(!joined) {
+            if (compatibleSolutions.isEmpty()) {
                 result.add(left);
             }
-
         }
-        return result;
+        return result.build();
     }
 
     public List<SolutionMapping> getJoin() {
         List<SolutionMapping> result = new ArrayList<>(leftSolutionSequence.size());
-        for(SolutionMapping left : leftSolutionSequence) {
-            for(SolutionMapping right : rightSolutionSequence) {
-                if(compatibilityChecker.isCompatible(left, right)) {
+        for (SolutionMapping left : leftSolutionSequence) {
+            for (SolutionMapping right : rightSolutionSequence) {
+                if (compatibilityChecker.isCompatible(left, right)) {
                     ImmutableMap.Builder<Variable, RDFTerm> combined = ImmutableMap.builder();
                     combined.putAll(left.asMap());
                     combined.putAll(right.asMap());
