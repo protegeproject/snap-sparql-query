a2b9ac6729f0503801027638c34e3729c4bf3373
Matthew Horridge <matthew.horridge@stanford.edu>
2017-10-31 13:51:45 -0700

    Removed commented out code

--- src/main/java/org/semanticweb/owlapi/sparql/api/AbstractVariable.java
+++ src/main/java/org/semanticweb/owlapi/sparql/api/AbstractVariable.java
@@ -14,26 +14,8 @@ import javax.annotation.Nonnull;
  */
 public abstract class AbstractVariable extends Variable {
 
-
-//    private final VariableNamePrefix prefix;
-//
-//    private final String variableName;
-
     public AbstractVariable(String variableName) {
         super(variableName);
-//        checkNotNull(variableName);
-//        if(variableName.startsWith(VariableNamePrefix.QUESTION_MARK.getPrefix())) {
-//            prefix = VariableNamePrefix.QUESTION_MARK;
-//            this.variableName = variableName.substring(1);
-//        }
-//        else if(variableName.startsWith(VariableNamePrefix.DOLLAR.getPrefix())) {
-//            prefix = VariableNamePrefix.DOLLAR;
-//            this.variableName = variableName.substring(1);
-//        }
-//        else {
-//            prefix = VariableNamePrefix.getDefault();
-//            this.variableName = variableName;
-//        }
     }
 
 
@@ -48,32 +30,6 @@ public abstract class AbstractVariable extends Variable {
         AbstractVariable other = (AbstractVariable) term;
         return this.getName().equals(other.getName());
     }
-//    public Set<Variable> getVariables() {
-//        return Collections.<Variable>singleton(this);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("Expression(");
-//        sb.append(getClass().getSimpleName());
-//        sb.append(" ?");
-//        sb.append(variableName);
-//        sb.append(")");
-//        return sb.toString();
-//    }
-//
-//    public String getIdentifier() {
-//        return variableName;
-//    }
-//
-//    public String getName() {
-//        return variableName;
-//    }
-//
-//    public VariableNamePrefix getVariableNamePrefix() {
-//        return prefix;
-//    }
 
     public AnnotationSubject toAnnotationSubject() {
         return new IRIVariable(getName());
@@ -86,10 +42,6 @@ public abstract class AbstractVariable extends Variable {
         }
         return term.get().evaluate(sm, evaluationContext);
     }
-
-//    public boolean canEvaluateAsBoolean(SolutionMapping sm) {
-//        return sm.isMapped(this) && sm.getTermForVariable(this).canEvaluateAsBoolean(sm);
-//    }
 
     public EvaluationResult evaluateAsEffectiveBooleanValue(SolutionMapping sm, AlgebraEvaluationContext evaluationContext) {
         Optional<RDFTerm> term = sm.getTermForVariable(this);
